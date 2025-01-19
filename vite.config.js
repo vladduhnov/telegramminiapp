e99e4dcb842e28fa7150dbd07110e039af165a30
vladduhnove <vlad@hellorobo.co>
2025-01-19 16:44:21 +0400

    Simplify build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
-  server: {
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
+    minify: false,
+    sourcemap: true
   }
 }) 
