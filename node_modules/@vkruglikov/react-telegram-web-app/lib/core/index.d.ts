45b982035148df582ff2dc10b2c34cffa37af5e8
vladduhnove <vlad@hellorobo.co>
2025-01-19 16:39:13 +0400

    Update build configuration

--- node_modules/@vkruglikov/react-telegram-web-app/lib/core/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as useSmoothButtonsTransition } from './useSmoothButtonsTransition';
-export { default as useWebApp } from './useWebApp';
-export { default as useAsyncMode } from './useAsyncMode';
-export * from './context';
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
 
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  server: {
+    port: 3000
   },
   resolve: {
     alias: {
